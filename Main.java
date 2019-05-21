4f7424799b5cec68f8008bc3035ebe7f3aef6aec
Ajulu Adigwe <ajuluadigwe@zipcodersMBP109.localdomain>
2019-05-21 16:59:05 -0400

    done

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
+        Scanner input= new Scanner (System.in);
+        System.out.print("What is your name?");
+        String name = input.nextLine();
+       
+        if (name.equals("Alice") || name.equals("Bob"));
+        System.out.println("Greetings "  + name); 
 
     }
 }
